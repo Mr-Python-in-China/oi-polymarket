71373ae665feb17605a923bf1c3ca95ad8500489
Mr. Python <mrpython@mrpython.top>
2026-08-11 19:29:38 +0000

    修改注册用户为阻塞锁

--- prisma/sql/register.sql
+++ prisma/sql/register.sql
@@ -1,20 +1,22 @@
 -- @param {String} $1:username
 -- @param {String} $2:avatar
 -- @param {String} $3:codeforcesSub
-
-WITH lock_acquired AS (
-  SELECT pg_try_advisory_xact_lock(21086) AS acquired
-),
-is_first_check AS (
-  SELECT NOT EXISTS (SELECT 1 FROM "User") AS is_first
-)
-INSERT INTO "User" ("username", "avatar", "codeforcesSub", "role")
-SELECT
-  $1,
-  $2,
-  $3,
-  CASE WHEN is_first_check.is_first THEN 'ADMIN'::"UserRole" ELSE 'USER'::"UserRole" END
-FROM lock_acquired
-CROSS JOIN is_first_check
-WHERE lock_acquired.acquired
-RETURNING "id";
+ WITH get_lock AS MATERIALIZED
+  (SELECT pg_advisory_xact_lock(21086)),
+      is_first_check AS
+  (SELECT NOT EXISTS
+     (SELECT 1
+      FROM "User") AS is_first)
+INSERT INTO "User" ("username",
+                    "avatar",
+                    "codeforcesSub",
+                    "role")
+SELECT $1,
+       $2,
+       $3,
+       CASE
+           WHEN is_first_check.is_first THEN 'ADMIN'::"UserRole"
+           ELSE 'USER'::"UserRole"
+       END
+FROM get_lock
+CROSS JOIN is_first_check RETURNING "id";
